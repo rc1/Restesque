7b25ab1ee30ea4a2407c08b74f30994f3fda1724
Ross Cairns <ross@electricglen.com>
2014-06-11 14:07:46 +0100

    Adding GUID

--- libs/packet.js
+++ libs/packet.js
@@ -91,7 +91,10 @@ Packet.prototype.getBody = function () {
 // ## Utils - Public
 
 Packet.makeToken = function () {
-    return Date.now() + Math.random();
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        v.toString(16);
+    });
 };
 
 // ## Constants 
